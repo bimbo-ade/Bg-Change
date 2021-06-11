35fe91339fd40ec89306dccc6e71380450fae281
bimbo-ade <bimboadeyemi65@gmail.com>
2021-06-11 16:04:33 +0100

    updated

---
+++ script.js
@@ -0,0 +1,41 @@
+const backArrow = document.getElementById('leftarrow');
+const frontArrow = document.getElementById('rightarrow');
+const imagess = document.getElementById('thumbnail');
+const img = [
+  { img: "tree.jpg" },
+  { img: "redflower.jpg" },
+  { img: "house.jpg" },
+  { img: "ocean.jpg" },
+  { img: "moutwt.jpg" }
+
+];
+let imgCount = 0;
+
+function images() {
+  const item = img[imgCount];
+  imagess.src = item.img;
+  document.body.style.backgroundImage = `url(${item.img})`;
+
+
+
+}
+backArrow.addEventListener('click', function () {
+  // var imgg = img[imgCount];
+  imgCount--
+  if (imgCount < 0) {
+    imgCount = img.length - 1
+  }
+
+  images();
+})
+
+frontArrow.addEventListener('click', function () {
+  //var imgg = img[imgCount];
+  imgCount++
+
+  if (imgCount > img.length - 1) {
+    imgCount = 0
+  }
+
+  images();
+})
